ab9c64ea4346573dc8d307ce3aa16329ae5eb426
ywonchall <ywonchall@163.com>
2023-02-27 07:27:59 +0000

    add pi camera picture

--- send.py
+++ send.py
@@ -1,8 +1,14 @@
 import requests
+from picamera import PiCamera
+
 
 img_path='./test-imgs/send_img.jpg'
-img = open(img_path,'rb')
-files = {"image":img}
-#访问服务
-res = requests.post("http://lcfclc.cn:10000/upload",files=files)
-print(res.text)
+camera = PiCamera()
+camera.start_preview()
+while True:
+    camera.capture(img_path)
+    img = open(img_path,'rb')
+    files = {"image":img}
+    # 访问服务
+    res = requests.post("http://lcfclc.cn:10000/upload",files=files)
+    print(res.text)
